c9a16c6c67d4521a91d92e4966667a2734b20f42
Rafael Quintela <rafael4sq@gmail.com>
2018-05-18 18:06:59 -0300

    audio app

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    extends: 'eslint:recommended',
+    globals: {module: false},
+    env: {
+        browser: true,
+    },
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        'react/jsx-uses-vars': ['error'],
+
+        'no-unused-vars': ['error', {varsIgnorePattern: '^h$'}],
+        'max-len': ['error', 120],
+        'indent': ['error', 4],
+        'linebreak-style': ['error', 'unix'],
+        'array-bracket-spacing': ['error', 'never'],
+        'object-curly-spacing': ['error', 'never'],
+        'comma-dangle': ['error', 'always-multiline'],
+        'no-extra-parens': ['error', 'all'],
+        'quotes': ['error', 'single'],
+        'semi': ['error', 'never'],
+        'quote-props': ['error', 'consistent-as-needed'],
+        'no-console': [0],
+    },
+}
